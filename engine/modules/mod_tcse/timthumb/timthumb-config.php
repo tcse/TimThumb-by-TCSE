9ffcc0886543382fd5efbbe99358e60b4ed77dd6
TCSE <talik@tcse-cms.com>
2021-02-26 08:09:53 +0300

    правки для true - false
    замена чекбокса на селект
    
    но все равно не передает данные в конфиг

--- engine/modules/mod_tcse/timthumb/timthumb-config.php
+++ engine/modules/mod_tcse/timthumb/timthumb-config.php
@@ -6,6 +6,7 @@
 * 	'{$config["timthumb_MAX_FILE_SIZE"]}'
 */
 
+
 //define('ALLOW_ALL_EXTERNAL_SITES', true);
 //define ('FILE_CACHE_TIME_BETWEEN_CLEANS', 86400);
 //define ('FILE_CACHE_MAX_FILE_AGE', 86400); 
@@ -20,4 +21,6 @@ define ('FILE_CACHE_MAX_FILE_AGE', '{$config["timthumb_FILE_CACHE_MAX_FILE_AGE"]
 
 //define ('MAX_FILE_SIZE', '{$config["timthumb_MAX_FILE_SIZE"]}');      
      
+
+
 ?>
